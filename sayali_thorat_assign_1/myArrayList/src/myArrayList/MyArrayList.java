96b467ebd931bc2efdb7e58b9ad8af49f7a1776c
Sayali Thorat <sthorat3@cs.binghamton.edu>
2019-06-17 09:04:55 -0400

    Driver class and MyArrayList class changes

--- sayali_thorat_assign_1/myArrayList/src/myArrayList/MyArrayList.java
+++ sayali_thorat_assign_1/myArrayList/src/myArrayList/MyArrayList.java
@@ -1,40 +1,77 @@
 package myArrayList;
 
-
 public class MyArrayList {
-
-	/*private int[] array ;
-
+	
+	private int MyArrayList[];
 
 	public MyArrayList() {
-		array = new int[50];
-	}
-
-void insertSorted(int newValue) {
-
-	}
-
-	void printInsertionOrder() {
+		// TODO Auto-generated constructor stub
+		MyArrayList=new int[50];
+		for (int i = 0; i < MyArrayList.length; i++) { 
+			MyArrayList[i]=-1;
+		}
 
 	}
+	public void insertSorted(int newValue) {
+		//
 
-	void removeValue(int value) {
-
+		int count=this.size();
+		//System.out.println("array size"+count);
+		int i;
+		for( i=0;i<count;i++){
+			if( MyArrayList[i]>newValue)
+				break;
+		}
+		for(int k=MyArrayList.length-2; k>=i; k--){
+			MyArrayList[k+1]=MyArrayList[k];            
+		}
+		MyArrayList[i]=newValue;
 	}
 
-	int indexOf(int value) {
-
-
+	public void printInsertionOrder() {
+		System.out.println("Inside print");
+		for (int i = 0; i<size(); ++i) { 
+			System.out.println(MyArrayList[i]);
+		}
+	}
+	public void removeValue(int value) {
+		for (int i = 0; i < MyArrayList.length; i++) {
+			if(MyArrayList[i]==value)
+			{
+				for (int j = i+1; j < MyArrayList.length; j++) {
+					int k=i;
+					MyArrayList[i]=MyArrayList[j];
+					k++;
+				}
+			}
+		}
+	}
+	public int size() {
+		int cnt=0;
+		for (int i = 0; i < MyArrayList.length && MyArrayList[i]!=-1; i++) {
+			cnt++;
+		}
+		return cnt;
+	}
+	public int sum() {
+		int sum=0;
+		for (int i = 0; i < size(); i++) {
+			sum=sum+MyArrayList[i];
+		}
+		return sum;
 
 	}
-
-	int size() {
-
-
+	public int indexOf(int value) {
+		int index=0;
+		for (int i = 0; i < MyArrayList.length && MyArrayList[i]!=-1; i++) {
+			if(MyArrayList[i]==value)
+			{
+				break;
+			}
+			index++;
+		}
+		return index;
 	}
 
-	int sum() {
-
-  }*/
 
 }
--- sayali_thorat_assign_1/myArrayList/src/myArrayList/driver/Driver.java
+++ sayali_thorat_assign_1/myArrayList/src/myArrayList/driver/Driver.java
@@ -1,78 +1,39 @@
 package myArrayList.driver;
+
 import java.io.BufferedReader;
-import java.io.BufferedWriter;
-import java.io.File;
 import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.io.IOException;
-import java.io.FileWriter;
-import java.util.Scanner;
 
+import myArrayList.MyArrayList;
 
-/**
- * @author Sayali Thorat
- *
- */
 public class Driver {
 
+	public static void main(String[] args) throws FileNotFoundException, IOException {
+		// TODO Auto-generated method stub
+		MyArrayList myArrayObject=new MyArrayList();
+		// TODO Auto-generated method stub
+		System.out.println("Test");
+		try(BufferedReader br = new BufferedReader(new FileReader("InputFile.txt"))) {
+			//StringBuilder sb = new StringBuilder();
+			String line = br.readLine();
 
-		public static void main(String[] args) {
-
-		/*
-		 * As the build.xml specifies the arguments as argX, in case the
-		 * argument value is not given java takes the default value specified in
-		 * build.xml. To avoid that, below condition is used
-		 */
-
-		/*if (args.length != 5 || args[0].equals("${arg0}") || args[1].equals("${arg1}") || args[2].equals("${arg2}")
-				|| args[3].equals("${arg3}") || args[4].equals("${arg4}")) {
-
-			System.err.println("Error: Incorrect number of arguments. Program accepts 5 argumnets.");
-			System.exit(0);
-		} */
-
-		System.out.println("Hello World! Lets get started with the assignment");
-
-
-		//to read input file
-		File file = new File("input.txt");
-
-		BufferedReader br = null;
-
-		try {
-				FileReader fr = new FileReader(file);
-				br = new BufferedReader(fr);
-
-				String line;
-
-				while( (line = br.readLine()) != null)
-				{
-					System.out.println(line);
-					//int number = Integer.parseInt(line);
-					//list.insertSorted(number);
-
-				}
-		} catch (FileNotFoundException e) {
-
-			System.out.println("File not found:" + file.toString());
-		}catch(IOException e) {
-
-			System.out.println("Unable to read file: " + file.toString());
-		}
-		finally {
-			try {
-				br.close();
-			}catch (IOException e) {
-				System.out.println("Unable to close file: " + file.toString());
+			while (line != null) {
+				//System.out.println(line);
+				myArrayObject.insertSorted(Integer.parseInt(line));
+				line = br.readLine();
 			}
-			catch(NullPointerException ex) {
-				//File was probably never opened !
-			}
+			// String everything = sb.toString();
 		}
-
-
-
-
-
-		}
+		myArrayObject.printInsertionOrder();
+		System.out.println("Index of element  "+myArrayObject.indexOf(1234));
+		System.out.println("Sum of array  "+myArrayObject.sum());
+		myArrayObject.removeValue(123);
+		myArrayObject.printInsertionOrder();
+		myArrayObject.removeValue(1234);
+		myArrayObject.printInsertionOrder();
+		myArrayObject.removeValue(12);
+		myArrayObject.printInsertionOrder();
+		System.out.println(myArrayObject.size());
+	}
 }
