803ce5fb6e07a1548ae358ed487fe2b5f084716d
Sayali Thorat <sthorat3@cs.binghamton.edu>
2019-06-17 14:42:08 -0400

    Util files updated and Driver class updated

--- sayali_thorat_assign_1/myArrayList/src/myArrayList/driver/Driver.java
+++ sayali_thorat_assign_1/myArrayList/src/myArrayList/driver/Driver.java
@@ -1,4 +1,34 @@
 package myArrayList.driver;
+import java.io.BufferedReader;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+import myArrayList.MyArrayList;
+import myArrayList.test.MyArrayListTest;
+import myArrayList.util.Results;
+
+public class Driver {
+public static void main(String[] args) throws FileNotFoundException, IOException {
+	if ( args.length != 3 )
+	{
+		System.out.println("Missing Input or Output File");
+		System.out.println("Run the program as  : ant -buildfile build.xml run -Darg0=infile.txt -Darg1=outfile.txt");
+		System.exit(0);
+	}
+	String inputFile = args[0];
+	String outputFile = args[1];
+	
+	
+	MyArrayList myArrayListObj = new MyArrayList();
+	Results resultObj = new Results();
+	MyArrayListTest testObj = new MyArrayListTest();
+
+	testObj.testObj(myArrayListObj, resultObj);
+	}
+}
+
+
+/*package myArrayList.driver;
 
 import java.io.BufferedReader;
 import java.io.FileNotFoundException;
@@ -7,14 +37,24 @@ import java.io.IOException;
 
 import myArrayList.MyArrayList;
 
-public class Driver {
 
+
+public class Driver {
+	
+	/*public static void main(String[] args) 
+	{
+		String Input_File;
+		
+		
+		
+	
+	
 	public static void main(String[] args) throws FileNotFoundException, IOException {
 		// TODO Auto-generated method stub
 		MyArrayList myArrayObject=new MyArrayList();
 		// TODO Auto-generated method stub
 		System.out.println("Test");
-		try(BufferedReader br = new BufferedReader(new FileReader("InputFile.txt"))) {
+		try(BufferedReader br = new BufferedReader(new FileReader("InputFile1.txt"))) {
 			//StringBuilder sb = new StringBuilder();
 			String line = br.readLine();
 
@@ -25,6 +65,11 @@ public class Driver {
 			}
 			// String everything = sb.toString();
 		}
+		
+		
+		
+		
+		
 		myArrayObject.printInsertionOrder();
 		System.out.println("Index of element  "+myArrayObject.indexOf(1234));
 		System.out.println("Sum of array  "+myArrayObject.sum());
@@ -35,5 +80,5 @@ public class Driver {
 		myArrayObject.removeValue(12);
 		myArrayObject.printInsertionOrder();
 		System.out.println(myArrayObject.size());
-	}
-}
+	} 
+} */
